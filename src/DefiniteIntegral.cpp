08470c55960e3349dee11e79fe1201981aa38dd1
Djan Carbonell <dcarbonell08@gmail.com>
2025-10-10 17:10:14 -0400

    Initial dummy value commit for definite integrals. Subsitute method needed.

--- src/DefiniteIntegral.cpp
+++ src/DefiniteIntegral.cpp
@@ -3,6 +3,42 @@
 //
 
 #include "Oasis/DefiniteIntegral.hpp"
+#include "Oasis/Integral.hpp"
 
 namespace Oasis {
+    
+    Integral<Expression>::IntegrateWithBounds(const Expression& integrand, const Expression& differential, const Expression& upper, const Expression& lower) const -> std::unique_ptr<Expression>
+    {
+        /*
+        Dependencies:
+            Oasis::Integral
+            Oasis::Subtract
+        */
+
+
+        Oasis::Integral interm {
+            *integrand,
+            *differential
+        };
+
+        /*
+            Currently evaluates the integral twice.
+            Need to look into way how to integrate only once and
+            copy values over.
+        */
+        auto upperBound = interm.Simplify();
+        auto lowerBound = interm.Simplify();
+
+        /*
+            Dummy values for now.
+        */
+        Oasis::Subtract answer {
+            *upperBoundSubstituted,
+            *lowerBoundSubstituted
+        };
+
+       return answer;
+    }
+
+
 } // Oasis
