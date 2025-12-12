e72b5ed9c591b494b77270f6fe597232aaaac808
Djan Carbonell <dcarbonell08@gmail.com>
2025-12-12 12:39:38 -0500

    Summation class for Oasis

--- include/Oasis/Summation.hpp
+++ include/Oasis/Summation.hpp
@@ -9,9 +9,55 @@
 
 namespace Oasis {
 
+template <IExpression OperandT, IExpression IncrementingVat, IExpression LowerBoundT, IExpression UpperBoundT>
+    class Summation;
+
+/// @cond 
 template <IExpression OperandT, IExpression IncrementingVarT, IExpression LowerBoundT, IExpression UpperBoundT>
 class Summation final : public BoundedBinaryExpression<Summation, OperandT, IncrementingVarT, LowerBoundT, UpperBoundT> {
+
+    Summation() = default;
+    Summation(const Expression<Expression, Expression, Expression, Expression>& other) = default;
+
+    Summation(const Expression& operand, const Expression& incrementingVar, const Expression& lower, const Expression& upper);
+
+    [[nodiscard]] auto Simplify() const -> std::unique_ptr<Expression> final;
+
+    EXPRESSION_TYPE(Summation)
+    EXPRESSION_CATEGORY(BoundedBinExp)
 };
+/// @endcond
+
+/**
+ * The Summation class acts as the sum of a sequence of numbers.
+ * 
+ * @section param Parameters:
+ * @tparam OperandT The expression to iterate through.
+ * @tparam IncrementingVarT The incrementing value of each successive sum.
+ * @tparam LowerBoundT The starting index of the summation.
+ * @tparam UpperBoundT The final index of the summation.
+ */
+template <IExpression OperandT = Expression, IExpression IncrementingVarT = Expression, IExpression LowerBoundT = Expression, IExpression UpperBoundT = Expression>
+class Summation : public BoundedBinaryExpression<OperandT,IncrementingVarT,LowerBoundT,UpperBoundT> {
+public:
+    Summation() = default;
+    Summation(const Summation<OperandT, IncrementingVarT, LowerBoundT, UpperBoundT>& other) 
+        : BoundedBinaryExpression<Summation, OperandT, IncrementingVarT, LowerBoundT, UpperBoundT>(other)
+    {
+    }
+
+    Summation(const Expression& operand, const Expression& incrementingVar, const Expression& lower, const Expression& upper)
+        : BoundedBinaryExpression<Summation,OperandT,IncrementingVarT,LowerBoundT,UpperBoundT>(operand,incrementingVar,lower,upper)
+    {
+    }
+
+
+    auto operator=(const Summation& other) -> Summation& = default;
+
+    EXPRESSION_TYPE(Summation)
+    EXPRESSION_CATEGORY(BoundedBinExp)
+}
+
 
 } // Oasis
 
--- src/Summation.cpp
+++ src/Summation.cpp
@@ -3,6 +3,37 @@
 //
 
 #include "Oasis/Summation.hpp"
+#include "Oasis/Add.hpp"
+#include "Oasis/Divide.hpp"
+#include "Oasis/Integral.hpp"
+#include "Oasis/Log.hpp"
+#include "Oasis/Matrix.hpp"
+#include "Oasis/Multiply.hpp"
+#include "Oasis/RecursiveCast.hpp"
+#include "Oasis/Variable.hpp"
 
 namespace Oasis {
+
+    Summation<Expression>::Summation(const Expression& operand, const Expression& incrementingVar, const Expression& lower, const Expression& upper)
+        : BoundedBinaryExpression(operand,incrementingVar,lower,upper)
+    {
+    }
+
+    auto Summation<Expression>::Simplify() const -> std::unique_ptr<Expression>
+    {
+        const auto simplifiedOp = mostSigOp ? mostSigOp->Simplify() : nullptr;
+        const auto simplifiedInc = leastSigOp ? leastSigOp->Simplify() : nullptr;
+
+        auto Oasis::Add result{
+            Oasis::Real{0},
+            Oasis::Real{0}
+        };
+
+        for(lower, lower < upper; lower += incrementingVar) {
+            auto resultant = Oasis::Add{result.Simplify(), lower};
+        }    
+
+        return resultant;
+    }
+
 } // Oasis
